0b29678edede1c4fbe458b237f42331e2c5b428d
Franco Emmanuel Herrera <herrerafranco694@gmail.com>
2026-04-23 10:28:29 -0300

    datos simulados

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // 🔥 ACTIVAR CORS
 builder.Services.AddCors();
-
+builder.WebHost.UseUrls("http://0.0.0.0:10000");
 var app = builder.Build();
 
 // 🔥 PERMITIR TODO (para desarrollo)
@@ -13,12 +13,19 @@ app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.MapGet("/stock", (IConfiguration config) =>
 {
-    using var conexion = new SqlConnection(
-        config.GetConnectionString("DefaultConnection"));
-
-    var lista = conexion.Query("SELECT * FROM StockQuimica");
+        var lista = new[]
+    {
+        new { Id = 1, Producto = "Cloro", Stock = 15 },
+        new { Id = 2, Producto = "Ácido", Stock = 8 }
+    };
 
     return Results.Ok(lista);
+    // using var conexion = new SqlConnection(
+    //     config.GetConnectionString("DefaultConnection"));
+
+    // var lista = conexion.Query("SELECT * FROM StockQuimica");
+
+    // return Results.Ok(lista);
 });
 
 app.Run();
